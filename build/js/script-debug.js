d1c549052b110561eef048fd62a789d656dd95c2
Leo Generali <leogen17@gmail.com>
2017-05-02 22:49:58 -0400

    Console.log result of DropBox's password analyzer

--- build/js/script-debug.js
+++ build/js/script-debug.js
@@ -1,5 +1,13 @@
 (function test( ){
-  var password = document.querySelector('.card__form');
+  var form = document.querySelector('.card__form');
 
-  console.log(password);
-});
+  form.addEventListener('keyup', analyze );
+
+  function analyze() {
+    var password = document.querySelector('.card__form').value;
+    var result = zxcvbn(password);
+    var score = result.score;
+  
+  }
+
+})();
