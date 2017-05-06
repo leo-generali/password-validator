028080dd92bbace8669f3b406174dc428a37fb99
Leo Generali <leogen17@gmail.com>
2017-05-06 14:35:44 -0400

    Add question mark when password does not exist

--- js/script-debug.js
+++ js/script-debug.js
@@ -7,31 +7,36 @@
     //To-add: Only do this is keypress is an alpha numeric value
     var password = document.querySelector('.card__form').value;
     var result = zxcvbn(password);
+    var length = password.length;
     var score = result.score;
-    changeEmoji(score);
+    changeEmoji(score, length);
   }
 
-  function changeEmoji(passwordScore) {
-    console.log(passwordScore);
+  function changeEmoji(passwordScore, passwordLength) {
     var emoji = document.querySelector('.emoji');
     var emojiParent = document.querySelector('.card--ouput');
     var emojiReplacement = returnNewEmoji(passwordScore);
     var newEmojiElem = createNewEmoji(emojiReplacement);
+    console.log(passwordLength);
 
     emojiParent.removeChild(emoji);
     emojiParent.appendChild(newEmojiElem);
 
     function returnNewEmoji(rating) {
-      if(rating === 0) {
-       return '💩';
-      }else if(rating === 1) {
-       return '🙁';
-      }else if(rating === 2) {
-       return '😐';
-      }else if(rating === 3) {
-       return '🙂';
-      }else {
-       return '😍';
+      if(passwordLength > 0) {
+        if(rating === 0) {
+         return '💩';
+        }else if(rating === 1) {
+         return '🙁';
+        }else if(rating === 2) {
+         return '😐';
+        }else if(rating === 3) {
+         return '🙂';
+        }else {
+         return '😍';
+        }
+      }else{
+        return '❓';
       };
     }
 
